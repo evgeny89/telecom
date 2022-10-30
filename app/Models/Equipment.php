93428187df9236e2271a7012d64772e4be4b50bb
evgeny <kas0733@gmail.com>
2022-10-30 19:06:27 +0500

    add phpdoc

--- app/Models/Equipment.php
+++ app/Models/Equipment.php
@@ -6,6 +6,26 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * App\Models\Equipment
+ *
+ * @property int $id
+ * @property int $equipment_type_id
+ * @property string $serial_number
+ * @property string $description
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\EquipmentType|null $type
+ * @method static \Illuminate\Database\Eloquent\Builder|Equipment newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Equipment newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Equipment query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Equipment whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Equipment whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Equipment whereEquipmentTypeId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Equipment whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Equipment whereSerialNumber($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Equipment whereUpdatedAt($value)
+ */
 class Equipment extends Model
 {
     use HasFactory;
--- app/Models/EquipmentType.php
+++ app/Models/EquipmentType.php
@@ -7,6 +7,27 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * App\Models\EquipmentType
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $mask
+ * @property string $pattern
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Equipment[] $equipments
+ * @property-read int|null $equipments_count
+ * @method static \Illuminate\Database\Eloquent\Builder|EquipmentType newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|EquipmentType newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|EquipmentType query()
+ * @method static \Illuminate\Database\Eloquent\Builder|EquipmentType whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|EquipmentType whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|EquipmentType whereMask($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|EquipmentType whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|EquipmentType wherePattern($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|EquipmentType whereUpdatedAt($value)
+ */
 class EquipmentType extends Model
 {
     use HasFactory;
